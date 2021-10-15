81bde39254d2b6f579579cc69068577df2eeb149
Maksim Meliashchuk <maksym873@gmail.com>
2021-10-15 18:13:39 +0300

    correcting of getting the path to files

--- src/main/java/com/epam/brest/reader/CsvReaderImpl.java
+++ src/main/java/com/epam/brest/reader/CsvReaderImpl.java
@@ -15,7 +15,7 @@ public class CsvReaderImpl implements Reader {
     @Override
     public BigDecimal getValueFromFile(BigDecimal value, String filePath) {
         URL url = getClass().getClassLoader().getResource(filePath);
-        
+
         try (Stream<String> stream = Files.lines(Paths.get(url.toURI()))) {
             BigDecimal bigDecimalPrice = BigDecimal.ZERO;
             BigDecimal bigDecimalRange;
